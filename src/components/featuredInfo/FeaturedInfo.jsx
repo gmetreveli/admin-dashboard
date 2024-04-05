81a3afb6d7726228dc2eda760f2d8169686dd94f
Giorgi Metreveli <g.metreveli1993@gmail.com>
2024-04-05 15:35:58 +0400

    upload

---
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -0,0 +1,41 @@
+import "./featuredInfo.css";
+import { ArrowDownward, ArrowUpward } from "@mui/icons-material";
+
+export default function FeaturedInfo() {
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <span className="featuredTitle">Revenue</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">$4,567</span>
+          <span className="featuredMoneyRate">
+            -12.3 <ArrowDownward />
+          </span>
+          <span className="featuredSub">Compared to last month</span>
+        </div>
+      </div>
+
+      <div className="featuredItem">
+        <span className="featuredTitle">Sales</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">$6,987</span>
+          <span className="featuredMoneyRate">
+            -1.47 <ArrowDownward />
+          </span>
+          <span className="featuredSub">Compared to last month</span>
+        </div>
+      </div>
+
+      <div className="featuredItem">
+        <span className="featuredTitle">Cost</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">$1,234</span>
+          <span className="featuredMoneyRate">
+            2.1 <ArrowUpward />
+          </span>
+          <span className="featuredSub">Compared to last month</span>
+        </div>
+      </div>
+    </div>
+  );
+}
